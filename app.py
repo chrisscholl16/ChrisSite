951f7a353e9cb33e4ced2afe0ee3c8d2a144f1de
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2024-09-02 18:27:49 -0400

    first route

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+
+# creating a flask instance (website)
+app = Flask(__name__)
+
+# creating a route (decorator)
+@app.route('/')
+def index():
+    return "<h1>Hello World!</h1>"
+
+if (__name__) == '__main__':
+    app.run(debug=True)
