2045345bffe1ca0966da0f03de63b5bde557ffa6
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2024-09-02 19:16:31 -0400

    Error pages

--- app.py
+++ app.py
@@ -6,7 +6,27 @@ app = Flask(__name__)
 # creating a route (decorator)
 @app.route('/')
 def index():
-    return "<h1>Hello World!</h1>"
+    first_name = "Chris"
+    stuff = "This is <strong>Bold</strong> Text"
+
+    favorite_pizza = ["Pepperoni", "Cheese", "Olives", 41]
+    return render_template("index.html", first_name=first_name, stuff=stuff, favorite_pizza=favorite_pizza)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", name=name)
+
+# creating custom error pages
+# invalid page
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
 
 if (__name__) == '__main__':
     app.run(debug=True)
