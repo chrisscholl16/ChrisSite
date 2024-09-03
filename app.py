00f27b5ec7de90e3d2cc8068e81f643880805246
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2024-09-02 22:01:03 -0400

    Static/Images/CSS

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # creating a flask instance (website)
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my secret key"
+
+# creating form class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # creating a route (decorator)
 @app.route('/')
 def index():
     first_name = "Chris"
     stuff = "This is <strong>Bold</strong> Text"
-
+    
     favorite_pizza = ["Pepperoni", "Cheese", "Olives", 41]
     return render_template("index.html", first_name=first_name, stuff=stuff, favorite_pizza=favorite_pizza)
 
@@ -26,6 +35,19 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+#creating name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    #validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully")
+    return render_template("name.html", name=name, form=form)
+
 
 
 if (__name__) == '__main__':
