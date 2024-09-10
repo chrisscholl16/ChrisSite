877c865944c5b9c204805fbbad9ca190329a4bfc
chrisscholl16 <135916179+chrisscholl16@users.noreply.github.com>
2024-09-10 19:19:38 -0400

    added post model

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, EqualTo, Length
-from datetime import datetime
+from datetime import datetime, date
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -25,6 +25,15 @@ app.config['SECRET_KEY'] = "my secret key"
 # initialize database
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# create a blog post model
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    author = db.Column(db.String(255))
+    date_posted = db.Column(db.Datetime, default=datetime.utcnow)
+    slug = db.Column(db.String(255))
 
 # create a model for database
 class Users(db.Model):
@@ -99,6 +108,11 @@ def update(id):
     else:
         return render_template("update.html", form=form, name_to_update=name_to_update, id=id)
 
+class PasswordForm(FlaskForm):
+    email = StringField("What's your email", validators=[DataRequired()])
+    password_hash = PasswordField("What's your Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 # creating form class
 class NamerForm(FlaskForm):
     name = StringField("What's your name", validators=[DataRequired()])
@@ -131,8 +145,7 @@ def index():
     first_name = "Chris"
     stuff = "This is <strong>Bold</strong> Text"
     
-    favorite_pizza = ["Pepperoni", "Cheese", "Olives", 41]
-    return render_template("index.html", first_name=first_name, stuff=stuff, favorite_pizza=favorite_pizza)
+    return render_template("index.html", first_name=first_name, stuff=stuff)
 
 @app.route('/user/<name>')
 def user(name):
@@ -148,6 +161,31 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+#creating password test page
+@app.route('/test_pw', methods=['GET', 'POST'])
+def test_pw():
+    email = None
+    password = None
+    pw_to_check = None
+    passed = None
+    form = PasswordForm()
+
+    #validate form
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password_hash.data
+
+        form.email.data = ''
+        form.password_hash.data = ''
+        
+        #looking up user by email in db
+        pw_to_check = Users.query.filter_by(email=email).first()
+
+        #check hash pass
+        passed = check_password_hash(pw_to_check.password_hash, password)
+
+    return render_template("test_pw.html", email=email, password=password, pw_to_check=pw_to_check, passed=passed, form=form)
 
 #creating name page
 @app.route('/name', methods=['GET', 'POST'])
